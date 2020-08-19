b18a6ffce63dc0f3976a9a4f412c223762a2343b
best <best.kittiporn@gmail.com>
2020-08-19 14:20:07 +0700

    commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,18 +3,19 @@ package main.java;
 public class main {
 
     public static void main(String[] args) {
+
         //ใช้แบบธรรมดา
         LambInterface str = new LambInterface() {
             @Override
             public String lanbTest(String name, String other) {
-                return "hi "+name + " "+other;
+                return "hi " + name + " " + other;
             }
         };
 
         //lambda
-        LambInterface str2 = (name,  other) ->"hi "+name + " "+other;
+        LambInterface str2 = (name, other) -> "hi " + name + " " + other;
 ี
-        System.out.println(str.lanbTest("best" , "เอง"));
+        System.out.println(str.lanbTest("best", "เอง"));
     }
 
 }
